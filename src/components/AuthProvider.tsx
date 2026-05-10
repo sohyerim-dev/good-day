3770197f99a1ecbeea2e3eb876bca56cde34300e
sohyerim-dev <fnmnn1@gmail.com>
2026-05-11 06:49:38 +0900

    리다이렉트 url 수정

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -37,7 +37,7 @@ export default function AuthProvider({
 
       if (autoLogin === "false" && !activeSession) {
         supabase.auth.signOut();
-        router.push("/login");
+        router.push(`/login?redirect=${encodeURIComponent(pathname)}`);
         return;
       }
       setUser({
